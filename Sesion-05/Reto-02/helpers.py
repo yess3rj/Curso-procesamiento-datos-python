bce075e1d6f4ae6c477668b9c6d6420953ea3da1
Ricardo Torres <rictor@cuhrt.com>
2021-07-05 12:24:41 -0500

    Se actualizan notas para ejemplo y reto 02 en Sesion-05, también se actualiza la celda de validación.

--- Sesion-05/Reto-02/helpers.py
+++
@@ -1,5 +0,0 @@
-
-def comparar_std(edades, std):
-    print(f'== Comparación Desviaciones Estándares ==\n')
-    print(f'Esperada: {edades.std()} - Recibida: {std}')
-    print(f'Cálculo {"Correcto... Felicidades!" if edades.std() == std else "Incorrecto... Intenta de nuevo"}')
